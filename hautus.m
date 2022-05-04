0177c6da2ca2ee3f301abf1b321708d0fa247ad3
followthwhiterabbit <62688580+followthwhiterabbit@users.noreply.github.com>
2022-05-04 17:10:20 +0200

    Update hautus.m

--- hautus.m
+++ hautus.m
@@ -4,38 +4,10 @@ function r = hautus(A, B, lambda)
 
 % use --> eye, size, rank 
 
-global  r hautus_matrix rank_control
+global  r hautus_matrix 
 
-
-
-
-hautus_matrix  = [lambda*eye(3) - A  B];
-
-
-
-rank_control = eig(A);
-
-
-if (lambda == rank_control(1)) |  (lambda == rank_control(2)) |  (lambda == rank_control(3) )
-    r  = 0; 
-    disp('rank deficient'); 
-else 
-    r = rank(lambda*eye(3) - A); 
-end 
-
-
-
-
-
-   
-%if lambda == rank_control(1) || lambda == rank_control(2) || lambda == rank_control(3) 
-   %  r = 0;
-%else   
- %   r = rank(lambda*eye(3) - A);
-%end 
-
-
-
-
+hautus_matrix  = [lambda*eye(3) - A,B];
  
+    r = rank(hautus_matrix);
+
 end 
